ece66849db7619c901e33686b9d93723cf20e453
Aaron <akr.git.receiver@gmail.com>
2016-02-10 12:28:06 -0600

    pushing all the things!

---
+++ javascripts/app.js
@@ -0,0 +1,20 @@
+jQuery(function($){
+
+  var $nav = $('.navItems');
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  //~~~~~~~~~~~~~~RandBGs~~~~~~~~~~~~~~~~
+  // need to add more bgs.
+  // var bgArr = ['one.jpg', 'two.jpg', 'three.jpg'];
+  // var bg = bgArr[Math.floor(Math.random() * bgArr.length)];
+  // var path = '/../imgs/';
+  // var imageUrl = path + bg;
+  //
+  // $('#content-wrapper').css('background-image', 'url(' + imageUrl +')');
+
+  //RANDOM COLORS
+  // var colArr = ['#000000', '#ef33fb', '#ef4555'];
+  // var col = colArr[Math.floor(Math.random() * colArr.length)];
+
+  
+
+});
---
+++ javascripts/preloader.js
@@ -0,0 +1,6 @@
+window.onload = function(){
+  var bdy = document.getElementById('earth'),
+      item = document.getElementById('preloader_1');
+      
+  bdy.removeChild(item);
+};
